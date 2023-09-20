51ea4951551b79f5b3343f96f93267de8ee74137
zenKmi <rodri.ml01@outlook.com>
2023-09-20 01:48:56 -0600

    PAN count and expiration date backend

--- src/App.js
+++ src/App.js
@@ -3,21 +3,44 @@ import React, { useEffect, useState } from 'react';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
+import { digitCountValidation } from './Utils';
+
 import './App.css';
+import dayjs from 'dayjs';
 
 function App() {
+
+  const [cardNumber, setCardNumber] = useState('');
+  const [errorText, setErrorText] = useState('');
+  const today = dayjs().add(1, 'month');
+
+  function handleInputChange(event){
+    const input = event.target.value;
+    const {cardNumber, errorText} = digitCountValidation(input);
+
+    setErrorText(errorText);
+    setCardNumber(cardNumber);
+  }
+
   return (
     <Container style={{ display: "flex", justifyContent: "center", height: "100vh", alignItems: 'center'}}>
       <Paper style={{ width: "35%", height: "50%", padding: "40px"}} elevation={7}>
         <Typography variant="h4" style={{ paddingBottom: '20px' }}>Payment Info</Typography>
+
         <hr></hr>
-        <TextField variant="standard" label="Card number" style={{ marginTop: "20px", marginBottom: "40px", width: '100%' }}></TextField>
+        
+        <TextField variant="standard" label="Card number" style={{ marginTop: "20px", marginBottom: "40px", width: '100%' }} value={cardNumber} onChange={handleInputChange} helperText={errorText} error={!!errorText}></TextField>
+        
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker label="MM/YY" views={["month", "year"]} style={{marginBottom: "40px"}}></DatePicker>
+          <DatePicker label="MM/YY" views={["month", "year"]} style={{marginBottom: "40px"}} minDate={today}></DatePicker>
         </LocalizationProvider>
-        <TextField variant="standard" label="CVV" style={{ marginBottom: "20px", width: '20%', marginLeft: '20px'}}></TextField>
-        <TextField variant="standard" label="Holder's name" style={{ marginBottom: "40px", width: "80%" }}></TextField>
+        
+        <TextField variant="standard" label="CVV/CVC" style={{ marginBottom: "20px", width: '20%', marginLeft: '20px'}}></TextField>
+        
+        <TextField variant="standard" label="Cardholder's name" style={{ marginBottom: "40px", width: "80%" }}></TextField>
+        
         <br></br>
+        
         <Button variant="contained">Pay now</Button>
       </Paper>
     </Container>
--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,51 @@
-export function digitCountValidation() {
-    
+export function digitCountValidation(input) {
+  if (input.length < 1){
+    return {
+      cardNumber: input,
+      errorText: '',
+    };
+  }
+  let clearInput = input.replace(/[^0-9]/g, "");
+
+  if (clearInput.length > 19) {
+    clearInput = clearInput.slice(0, 19);
+  }
+
+  if (clearInput.length >= 16 && clearInput.length <= 19) {
+    return { cardNumber: clearInput, errorText: "" };
+  } else {
+    return {
+      cardNumber: clearInput,
+      errorText:
+        "Primary Account Number (PAN) / Credit card number must be between 16 to 19 numbers long.",
+    };
+  }
+}
+
+export function cvvCountValidation(input) {
+  if (input.length < 1) {
+    return {
+      cardNumber: input,
+      errorText: "",
+    };
+  }
+  let clearInput = input.replace(/[^0-9]/g, "");
+
+  if (clearInput.length > 4) {
+    clearInput = clearInput.slice(0, 4);
+  }
+
+  if (clearInput.length >= 3 && clearInput.length <= 4) {
+    return { cardNumber: clearInput, errorText: "" };
+  } else {
+    return {
+      cardNumber: clearInput,
+      errorText:
+        "CVV/CVC must be between 3 to 4 numbers long.",
+    };
+  }
+}
+
+export function checkCardNetwork(){
+
 }
