332400d9c44278f27ba88022643d3b3b734ca861
zenKmi <rodri.ml01@outlook.com>
2023-09-20 19:23:21 -0600

    Luhn's algorithm, and network checker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Container, Bottom, TextField, Typography, Paper, Button } from '@mui/material';
+import { Container, Bottom, TextField, Typography, Paper, Button, Card } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
-import { cvvCountValidation, digitCountValidation } from './Utils';
+import { CVVCountValidation, PANCountValidation, CardNetworkCheck, LuhnsAlgorithmLastDigitCheck } from './Utils';
 
 import './App.css';
 import dayjs from 'dayjs';
@@ -12,25 +12,56 @@ function App() {
 
   const [cardNumber, setCardNumber] = useState('');
   const [errorText, setErrorText] = useState('');
-  const [cvvError, setCVVError] = useState("");
+  const [cvvError, setCVVError] = useState('');
+  const [isAmericanExpress, setIsAmericanExpress] = useState('');
   const [cvv, setCVV] = useState('');
   const today = dayjs().add(1, 'month');
 
+  let cardNumberValid = false;
+  let cvvValid = false;
+  let cardHolderValid = false;
+
   function handleInputChange(event) {
     const input = event.target.value;
-    switch (event.target.id) {
-      case "cardNumber":
-        const { cardNumber, errorText } = digitCountValidation(input);
+    if (event.target.id === "cardNumber"){
+      const { cardNumber, errorText } = PANCountValidation(input);
 
-        setErrorText(errorText);
-        setCardNumber(cardNumber);
-        break;
-      case "cvv":
-        const { cvv, cvvError } = cvvCountValidation(input);
+      setIsAmericanExpress(CardNetworkCheck(input));
 
-        setCVV(cvv);
-        setCVVError(cvvError);
-        break;
+      if (!CardNetworkCheck(input)) setCVV(cvv.slice(0, 3));
+
+      cardNumberValid = (input.length >= 16) ? LuhnsAlgorithmLastDigitCheck(cardNumber) : false;
+
+      setErrorText(errorText);
+      setCardNumber(cardNumber);
+
+    } else if (event.target.id === "cvv"){
+      const { cvv, cvvError } = CVVCountValidation(input, isAmericanExpress);
+
+      if (cvv.length == (isAmericanExpress) ? 4 : 3){
+        cvvValid = true;
+      } else {
+        cvvValid = false;
+      }
+
+      setCVV(cvv);
+      setCVVError(cvvError);
+
+    } else if (event.target.id === "cardholder"){
+      if (input.length >= 1){
+        cardHolderValid = true;
+      } else {
+        cardHolderValid = false;
+      }
+    }
+  }
+
+  function handlePayNow(event) {
+    console.log(":D")
+    if (cardNumberValid && cvvValid && cardHolderValid){
+      console.log("Todo nice :D");
+    } else {
+      console.log(";-;");
     }
   }
 
@@ -41,7 +72,7 @@ function App() {
 
         <hr></hr>
         
-        <TextField id="cardNumber" variant="standard" label="Card number" style={{ marginTop: "20px", marginBottom: "40px", width: '100%' }} value={cardNumber} onChange={handleInputChange} helperText={errorText} error={!!errorText}></TextField>
+        <TextField id="cardNumber" variant="standard" label="Card number" style={{ marginTop: "20px", marginBottom: "40px", width: '100%' }} autoFocus={true} value={cardNumber} onChange={handleInputChange} helperText={errorText} error={!!errorText}></TextField>
         
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker label="MM/YY" views={["month", "year"]} style={{marginBottom: "40px"}} minDate={today} slotProps={{textField: {disabled: true}}}/>
@@ -49,11 +80,11 @@ function App() {
         
         <TextField id="cvv" variant="standard" label="CVV/CVC" style={{ marginBottom: "20px", width: '20%', marginLeft: '20px'}} value={cvv} onChange={handleInputChange} helperText={cvvError} error={!!cvvError}></TextField>
         
-        <TextField variant="standard" label="Cardholder's name" style={{ marginBottom: "40px", width: "80%" }}></TextField>
+        <TextField id="cardholder" variant="standard" label="Cardholder's name" style={{ marginBottom: "40px", width: "80%" }} onChange={handleInputChange}></TextField>
         
         <br></br>
         
-        <Button variant="contained">Pay now</Button>
+        <Button variant="contained" onClick={handlePayNow}>Pay now</Button>
       </Paper>
     </Container>
   );
--- src/Utils.js
+++ src/Utils.js
@@ -1,4 +1,4 @@
-export function digitCountValidation(input) {
+export function PANCountValidation(input) {
   if (input.length < 1){
     return {
       cardNumber: input,
@@ -22,7 +22,7 @@ export function digitCountValidation(input) {
   }
 }
 
-export function cvvCountValidation(input) {
+export function CVVCountValidation(input, isCVVLonger) {
   if (input.length < 1) {
     return {
       cvv: input,
@@ -31,21 +31,51 @@ export function cvvCountValidation(input) {
   }
   let clearInput = input.replace(/[^0-9]/g, "");
 
-  if (clearInput.length > 4) {
-    clearInput = clearInput.slice(0, 4);
+  const maxLength = (isCVVLonger)? 4 : 3;
+
+  if (clearInput.length > maxLength) {
+    clearInput = clearInput.slice(0, maxLength);
   }
 
-  if (clearInput.length >= 3 && clearInput.length <= 4) {
+  if (clearInput.length <= maxLength) {
     return { cvv: clearInput, cvvError: "" };
   } else {
     return {
       cvv: clearInput,
-      cvvError:
-        "CVV/CVC must be between 3 to 4 numbers long.",
+      cvvError: "CVV/CVC must be " + maxLength + " numbers long.",
     };
   }
 }
 
-export function checkCardNetwork(){
+export function CardNetworkCheck(input){
+  const firstSixDigits = input.slice(0, 6);
 
+  if (/^3[47]/.test(firstSixDigits)) { //American Express cards start with 34 or 37
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function LuhnsAlgorithmLastDigitCheck(input){
+  const trimmedInput = input.slice(0, input.length - 1);
+  const reversedCardNumber = trimmedInput.split('').reverse().join('');
+  let sum = 0;
+
+  for (let i = 0; i < reversedCardNumber.length; i++){
+    let digit = parseInt(reversedCardNumber[i], 10);
+
+    if (i % 2 === 0){
+      digit *= 2;
+    }
+
+    if (digit >= 10) {
+      digit -= 9;
+    }
+
+    sum += digit;
+  }
+   const lastDigit = (sum * 9) % 10;
+
+   return (lastDigit === input.slice(input.length - 4, input.length) % 10) //sliced input, since JS cannot handle mod on large numbers
 }
